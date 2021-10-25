ea43e00a875eb557372cbc95a46a74a817f38a59
Carter Vande Moore <72159057+cvmoore@users.noreply.github.com>
2021-10-25 14:14:06 -0400

    Last_two written

--- code.ml
+++ code.ml
@@ -6,3 +6,14 @@ let rec last list =
     h::t -> last t
     |
     [] -> None
+
+(*Returns the last two elements of a list *)
+let rec last_two list = 
+    match list with
+    [h] -> None
+    |
+    [h;h1] -> Some (h, h1)
+    |
+    h::t -> last_two t
+    |
+    [] -> None
